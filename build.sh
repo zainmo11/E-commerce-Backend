c4c2ed7ba3f294ef5a29a7829a74dd19fab3b87e
Abdelrahman Hamada <69439342+abdlrhman08@users.noreply.github.com>
2024-04-25 00:15:57 +0200

    workaround to create superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python ./manage.py createsuperuser --no-input
+fi
